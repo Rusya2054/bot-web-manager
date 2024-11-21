9d0b79b28afe0196328725fe5cde7134c1fdadf6
Rusya2054 <Rusya2054@mail.ru>
2024-11-21 22:40:31 +0500

    -added creating of specialist to database

--- src/main/java/com/Rusya2054/bot/web/manager/controllers/management/SpecialistController.java
+++ src/main/java/com/Rusya2054/bot/web/manager/controllers/management/SpecialistController.java
@@ -1,14 +1,20 @@
 package com.Rusya2054.bot.web.manager.controllers.management;
 
 import com.Rusya2054.bot.web.manager.models.management.Specialist;
+import com.Rusya2054.bot.web.manager.models.management.SpecialistImage;
 import com.Rusya2054.bot.web.manager.services.management.SpecialistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,19 +24,30 @@ public class SpecialistController {
     private final SpecialistService specialistService;
 
     @GetMapping
-    public String specialistsPage(){
-        System.out.println("specialistsPage");
+    public String specialistsPage(Model model){
+        model.addAttribute("specialists", specialistService.getAll());
         return "specialists";
     }
 
     @PostMapping("/add")
-    public void addSpecialist(Specialist specialist){
-        System.out.println(specialist);
+    public String addSpecialist(MultipartFile file, Specialist specialist) throws IOException {
+        if (!file.getOriginalFilename().isEmpty()){
+            SpecialistImage specialistImage = specialistService.toSpecialistImageEntity(file);
+            specialist.setImage(specialistImage);
+        }
+        specialistService.createSpecialist(specialist);
+        return "redirect:/specialist";
     }
+
     @GetMapping("/{id}")
-    public void getSpecialist(@PathVariable Long id){
-        System.out.println(id);
-        System.out.println(specialistService.getSpecialistById(id));
+    public String getSpecialist(@PathVariable Long id, Model model){
+        try {
+            Specialist specialist = specialistService.getSpecialistById(id).orElseThrow();
+            model.addAttribute("specialist", specialist);
+            return "specialist-info";
+        } catch (NoSuchElementException noSuchElementException){
+            return "redirect:/specialist";
+        }
     }
 
     @PostMapping("/update?{id}")
--- src/main/java/com/Rusya2054/bot/web/manager/services/management/SpecialistService.java
+++ src/main/java/com/Rusya2054/bot/web/manager/services/management/SpecialistService.java
@@ -1,17 +1,23 @@
 package com.Rusya2054.bot.web.manager.services.management;
 
 import com.Rusya2054.bot.web.manager.models.management.Specialist;
+import com.Rusya2054.bot.web.manager.models.management.SpecialistImage;
 import com.Rusya2054.bot.web.manager.repositories.management.SpecialistImageRepository;
 import com.Rusya2054.bot.web.manager.repositories.management.SpecialistRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SpecialistService {
+    private static final Logger log = LoggerFactory.getLogger(SpecialistService.class);
     private final SpecialistRepository specialistRepository;
     private final SpecialistImageRepository specialistImageRepository;
 
@@ -20,5 +26,22 @@ public class SpecialistService {
     }
     public List<Specialist> getAll(){
         return specialistRepository.findAll();
+    }
+
+    public SpecialistImage toSpecialistImageEntity(MultipartFile file) throws IOException {
+        final SpecialistImage image = new SpecialistImage();
+        image.setName(file.getName());
+        image.setOriginalFileName(file.getOriginalFilename());
+        image.setContentType(file.getContentType());
+        image.setSize(file.getSize());
+        image.setBytes(file.getBytes());
+        return image;
+    }
+
+    public boolean createSpecialist(Specialist specialist){
+        specialist.setActive(true);
+        log.info("Created new specialist with id: {}", specialist.getId());
+        specialistRepository.save(specialist);
+        return true;
     }
 }
